956537603a6c72c659aa8fab3244f0c63a84648b
Rob Hill <robert.hill731@gmail.com>
2023-01-26 12:44:12 -0500

    add state to result, remove unused css

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './Result.module.css';
 
 export class Result extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: null };
+  }
+
   render() {
     return (
-      <div>{this.props.value}</div>
+      <div>{this.state.value}</div>
     );
   }
 }
