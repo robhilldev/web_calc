bddc88ce1a8da830fa96fc709ea2f797d885c884
Rob Hill <robert.hill731@gmail.com>
2023-02-20 13:10:19 -0500

    cleanup and reorganize auto generated files, make result box taller

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './components/App';
+import App from '../components/App';
 
 test('renders web_calc title, calculator table, and footer', () => {
   render(<App />);
